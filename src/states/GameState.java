fd712a0e154d96f0edcd6402e188cb37c4b5465f
Quint <qbark009@gmail.com>
2016-04-21 00:40:10 -0500

    Building of world map generator is complete

--- src/states/GameState.java
+++ src/states/GameState.java
@@ -1,6 +1,7 @@
 package states;
 
 import Entities.Actors.Player;
+import Rooms.World;
 import Tiles.Tile;
 import graphic_launch.Assets;
 import main_pack.Game;
@@ -12,20 +13,24 @@ import java.awt.*;
  */
 public class GameState extends State {
     private Player player;
+    private World world;
     public GameState(Game game){
         super(game);
         player=new Player(game, 100, 100);
+        world = new World("assets/rooms/room1.txt");
     }
 
     @Override
     public void tick() {
+        world.tick();
         player.tick();
     }
 
     @Override
     public void render(Graphics g) {
-        //g.drawImage(Assets.player,0,0,null);
+        world.render(g);
+        // g.drawImage(Assets.player,0,0,null);
         player.render(g);
-        Tile.tiles[0].render(g, 0, 0);
+        //Tile.tiles[0].render(g, 0, 0);
     }
 }
